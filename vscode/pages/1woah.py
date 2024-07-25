448fc2e906318076437a73bbb3c4729d2e782b3c
OctopusRobot21 <octopusrobotgaming@gmail.com>
2024-07-24 21:29:34 -0700

    Update 1woah.py

--- vscode/pages/1woah.py
+++ vscode/pages/1woah.py
@@ -2,10 +2,6 @@ import streamlit as st
 import requests as req
 import os
 
-def get_image_path(image_name):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(script_dir, "./images", image_name)
-
 st.set_page_config(page_title="hey im a page", page_icon="👺")
 
 st.sidebar.header("hey im a page")
@@ -17,14 +13,9 @@ st.subheader("how awesome am i?????")
 liker = st.slider('liker') 
 if(liker==100):
     st.write("heck yes dude!")
-    yes_path = get_image_path("heckyes.jfif")
-
-    if os.path.exists(yes_path):
-        st.image(yes_path)
-    else:
-        st.write("Image not found!")
+    st.header("👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍")
 else:
-    st.write("no")
+    st.write("boooooooooooooooooooooooooooooooooo ur wrong")
 
 
 
