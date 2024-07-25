6b58c087c968952cc7bd945590e014f599e8f45e
OctopusRobot21 <octopusrobotgaming@gmail.com>
2024-07-24 21:18:20 -0700

    Update About_Me.py

--- vscode/About_Me.py
+++ vscode/About_Me.py
@@ -2,6 +2,10 @@ import streamlit as st
 import requests as req
 import os
 
+def get_image_path(image_name):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(script_dir, "images", image_name)
+
 st.set_page_config(
     page_title="matthew",
     page_icon="👹",
@@ -12,17 +16,10 @@ st.sidebar.header("woah theres another page")
 st.header("hey fellas im matthew")
 
 'i like video game'
+game_path = get_image_path("game.png")
 
-# Get the directory of the current script
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Construct the path to the image
-image_path = os.path.join(script_dir, "images/game.png")
-
-st.write("Image path:", image_path)
-
-if os.path.exists(image_path):
-    st.image(image_path)
+if os.path.exists(game_path):
+    st.image(game_path)
 else:
     st.write("Image not found!")
 
@@ -31,9 +28,19 @@ r = req.get("https://www.youtube.com/watch?v=4L7u4F4cGY0")
 st.video(r.url)
 
 'i like anime'
+op_path = get_image_path("onepiece.png")
+
+if os.path.exists(op_path):
+    st.image(op_path)
+else:
+    st.write("Image not found!")
 #st.image("images/onepiece.png")
 
 st.header("I am the fattest man trapped in a skinny body")
 
-#if st.checkbox('duck'):
-    #st.image("images/duck.jfif")
+if st.checkbox('duck'):
+    duck_path = get_image_path("duck.jfif")
+    if os.path.exists(duck_path):
+        st.image(duck_path)
+    else:
+        st.write("Image not found!")
